3df969c430bb9de5d8f0d4a5a029c0e9716a6fe4
ark2017 <31368243+ark2017@users.noreply.github.com>
2017-08-26 23:36:44 +0800

    conflictest

--- chirLine/src/test/java/com/chirline/Test/CommitToGitTest.java
+++ chirLine/src/test/java/com/chirline/Test/CommitToGitTest.java
@@ -3,8 +3,9 @@ package com.chirline.Test;
 public class CommitToGitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		if("1".equals("confligtest")){
+			
+		}
 	}
 
 }
