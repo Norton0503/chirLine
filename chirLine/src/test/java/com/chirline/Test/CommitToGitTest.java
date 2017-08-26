fee6bdd4318d4ee1d9b77d5452ceef965428aafb
guanwanli <695774200@qq.com>
2017-08-26 23:49:26 +0800

    冲突问题

---
+++ chirLine/src/test/java/com/chirline/Test/CommitToGitTest.java
@@ -0,0 +1,14 @@
+package com.chirline.Test;
+
+public class CommitToGitTest {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		System.out.print("hello world2");
+
+		System.out.println("hello github");
+
+	}
+
+}
