68f58c09b6b768b0a794cf965e7a7efed705eb53
guanwanli <695774200@qq.com>
2017-08-27 00:02:02 +0800

    gg

--- chirLine/src/test/java/com/chirline/Test/CommitToGitTest.java
+++ chirLine/src/test/java/com/chirline/Test/CommitToGitTest.java
@@ -5,7 +5,7 @@ public class CommitToGitTest {
 	public static void main(String[] args) {
 
 		// TODO Auto-generated method stub
-		System.out.print("hello world2");
+		System.out.print("hello world3");
 
 		System.out.println("hello github");
 
